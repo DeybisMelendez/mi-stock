6abaf5cab0de927acd14abe7a567db8f102d721d
DeybisMelendez <damv2007@gmail.com>
2026-02-05 13:46:16 -0600

    agregado configuración para deploy

--- mistock/settings.py
+++ mistock/settings.py
@@ -9,23 +9,25 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(dotenv_path=".secret")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9x4k20omn%n5twbyuc60y1oy8^!85k%xyv^1lxdu%5ir@dhf-4'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'django-insecure-9x4k20omn%n5twbyuc60y1oy8^!85k%xyv^1lxdu%5ir@dhf-4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
+DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
