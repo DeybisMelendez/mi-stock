9db07cc51d977f515fb7bde817f1e63daecced8f
DeybisMelendez <damv2007@gmail.com>
2026-02-05 10:51:38 -0600

    mejorado listado generico

--- stock/templatetags/getattribute.py
+++ stock/templatetags/getattribute.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import datetime, date as date_type
 register = template.Library()
 
 
@@ -12,5 +13,26 @@ def getattribute(obj, attr):
         for part in attr.split("__"):
             obj = getattr(obj, part)
         return obj
+    except Exception:
+        return ""
+
+
+@register.filter
+def format_value(obj, attr):
+    """
+    Obtiene el atributo y lo formatea si es una fecha/hora.
+    """
+    try:
+        for part in attr.split("__"):
+            obj = getattr(obj, part)
+        
+        # Formatear si es datetime o date
+        if isinstance(obj, datetime):
+            return obj.strftime("%d/%m/%Y %H:%M")
+        elif isinstance(obj, date_type):
+            return obj.strftime("%d/%m/%Y")
+        
+        # Convertir a string para otros tipos
+        return str(obj) if obj is not None else ""
     except Exception:
         return ""
